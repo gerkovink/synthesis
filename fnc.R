9245cebaf73d871dd4b9777b3dc71b0e60d5a585
Gerko Vink <g.vink@uu.nl>
2023-02-22 11:51:54 +0100

    Update fnc.R

--- fnc.R
+++ fnc.R
@@ -22,3 +22,49 @@ synplot <- function(syn.obj, lm.formula){
 }
 
 
+pred <- make.predictorMatrix(boys)
+meth <- make.method(boys)
+pred[c("hgt", "wgt"), "bmi"] <- 0
+meth["bmi"] <- "~ I(wgt/(hgt/100)^2)"
+imp <- mice(boys, 
+            m=1, 
+            maxit = 10,
+            pred = pred, 
+            meth = meth, 
+            print = FALSE)
+truth <- complete(imp)
+
+syn <- mice(truth, 
+            meth = "cart", 
+            m = 5, 
+            maxit = 1, 
+            print = FALSE,
+            where = matrix(TRUE, nrow(truth), ncol(truth)))
+
+model <- complete(syn, "all") %>% map(~.x %$% lm(age ~ bmi + hc))
+
+synplot <- function(model.list, smoother = "lm"){
+  fit <- model.list[[1]]
+  form <- formula(fit)
+  vars <- form %>% all.vars()
+  # row-bind all dfs
+  mice::complete(mids, "long") %>% 
+    # suppose lm.formula contains one predictor x
+    ggplot(aes(x = fit$fitted.values, 
+               y = vars[1], 
+               color= .imp)) +
+    geom_point(size = 1, alpha = 0.2) +
+    geom_line(stat = "smooth", 
+              method = smoother, 
+              alpha = 0.5, 
+              position = position_dodge(width = 1)) +
+    theme_classic() +
+    labs(title = paste("Line for", deparse(form)), color="")
+}
+
+A <- model %>% map(~.x[c("fitted.values", "model")] %>% 
+                do.call("cbind", .) %>% 
+                select(c(1,2))) %>% 
+  do.call("rbind", .) %>% 
+  mutate(imp = rep(1:length(model), rep(dim(.)[[1]]/length(model), length(model))))
+
